4426349a369968b038574010bb9ed0e006b026a4
abdulhadi lbabidi <141328373+abdulhadi-lbabidi@users.noreply.github.com>
2026-01-28 14:47:02 +0300

    reward and payment

---
+++ app/Http/Requests/Payment/CreateLoanRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Payment;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateLoanRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'employee_id'=>'required|exists:employees,id',
+            'admin_id'=>'required|exists:admins,id',
+            'amount'=>'required|numeric',
+            'paid_amount'=>'required|numeric',
+            'role'=>'required|in:قيد الانتظار , مدفوعة جزئياً, مسددة بالكامل',
+            'date'=>'required|date',
+        ];
+    }
+}
---
+++ app/Http/Services/RewardService.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace App\Http\Services;
+
+use App\Models\Reward;
+
+class RewardService
+{
+    public function getAll()
+    {
+        return Reward::whereNull('deleted_at')
+            ->get();
+    }
+
+    public function getArchived()
+    {
+        return Reward::onlyTrashed()
+            ->get();
+    }
+
+
+    public function create(array $data)
+    {
+        return Reward::create($data);
+    }
+
+    public function update(Reward $reward, array $data)
+    {
+        $reward->update($data);
+        return $reward;
+    }
+
+    public function delete(Reward $reward)
+    {
+        return $reward->delete();
+    }
+
+    public function forceDelete(Reward $reward)
+    {
+        return $reward->forceDelete();
+    }
+
+    public function restore(Reward $reward)
+    {
+        return $reward->restore();
+    }
+}
