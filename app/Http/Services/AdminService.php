4864447cf33cd6f150b7097339546f6e4bdba53e
AliMohammadDev <aloshmohammad2001@gmail.com>
2026-01-29 11:22:01 +0300

    fixed in admin

--- app/Http/Services/AdminService.php
+++ app/Http/Services/AdminService.php
@@ -52,16 +52,16 @@ class AdminService
             'name' => $data['name'] ?? $admin->name,
         ]);
 
-        if ($admin->users) {
-            $admin->users->update([
-                'full_name' => $data['full_name'] ?? $admin->users->full_name,
-                'phone_number' => $data['phone_number'] ?? $admin->users->phone_number,
-                'email' => $data['email'] ?? $admin->users->email,
-                'password' => isset($data['password']) ? Hash::make($data['password']) : $admin->users->password,
+        if ($admin->user) {
+            $admin->user->update([
+                'full_name' => $data['full_name'] ?? $admin->user->full_name,
+                'phone_number' => $data['phone_number'] ?? $admin->user->phone_number,
+                'email' => $data['email'] ?? $admin->user->email,
+                'password' => isset($data['password']) ? Hash::make($data['password']) : $admin->user->password,
             ]);
         }
 
-        return $admin->load('users');
+        return $admin->load('user');
     }
 
     public function delete(Admin $admin)
@@ -89,7 +89,7 @@ class AdminService
             $admin->user()->withTrashed()->restore();
         }
 
-        return $admin->load('users');
+        return $admin->load('user');
     }
 
 
